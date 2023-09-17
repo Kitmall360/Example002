3211b408b6be1efdad7fb46b8cdef3b709e9ff1b
Simon <Simon-3000@yandex.ru>
2023-09-17 21:57:58 +0300

    Изменили регистр

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите Имя пользователя ");
 string username = Console.ReadLine();
 
-if(username == "Катя")
+if(username.ToLower() == "катя")
 {
     Console.WriteLine("Оо госпожа!!");
 }
