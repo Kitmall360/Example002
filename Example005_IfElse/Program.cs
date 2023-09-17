3869a267942c90483ddf3b53e43af3b9fbc87b28
Simon <Simon-3000@yandex.ru>
2023-09-17 21:53:45 +0300

    Выборка по имени

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Катя")
+{
+    Console.WriteLine("Оо госпожа!!");
+}
+else
+{
+    Console.Write("Дарова, Холоп ");
+    Console.WriteLine(username);
+}
